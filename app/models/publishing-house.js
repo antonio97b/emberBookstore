0a22f51496750a59b4f8556808b098157430d2be
Tony <tony.navarro@gmail.com>
2016-09-14 10:48:21 -0700

    - Add and configure Book, Author, Publisher, and publishing-house model - Add and configure application JSONAPIAdapter.

---
+++ app/models/publishing-house.js
@@ -0,0 +1,11 @@
+import Publisher from './publisher';
+
+export default Publisher.extend();
+
+// import DS from 'ember-data';
+
+// export default DS.Model.extend({
+//   name: DS.attr('string'),
+//   discount: DS.attr('number'),
+//   books: DS.hasMany('book')
+// });
